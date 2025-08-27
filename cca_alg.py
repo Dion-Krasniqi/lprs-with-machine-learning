940db781441c60daba7bf54d814923b14dc15b71
Dion-Krasniqi <dion.krasniqi6@student.uni-pr.edu>
2025-08-27 14:21:25 +0200

    Polished CCA algorithm to only draw over license plate like regions based on the dimensions of the image and condition that width is greater than height

--- cca_alg.py
+++ cca_alg.py
@@ -5,6 +5,13 @@ import matplotlib.patches as patches
 import grayscale
 
 label_image = measure.label(grayscale.binary_car_image) # labels each connected component
+
+# defines the min and max dimensions that the plate can be based on the images size
+plate_dimesions = (0.08*label_image.shape[0], 0.2*label_image.shape[0], 0.15*label_image.shape[1], 0.4*label_image.shape[1])
+min_height, max_height, min_width, max_width = plate_dimesions
+plate_objects_cordinates = [] # list to keep the box coordinates of the filtered objects
+plate_like_objects = [] # binary image of just the filtered objects
+
 fig, (ax1) = plt.subplots(1)
 ax1.imshow(grayscale.gray_car_image, cmap = "gray")
 
@@ -13,8 +20,14 @@ for region in regionprops(label_image):
         continue         # said area has less than 50 pixels, most likely noise/too small, so skip
 
     minRow, minCol, maxRow, maxCol = region.bbox # this is another property of the selected region, basically the bounding box
-    rectBorder = patches.Rectangle((minCol, minRow), maxCol - minCol, maxRow - minRow, edgecolor = "red", linewidth = 2, fill = False)
-    # creates rectangle with said proportions
-    ax1.add_patch(rectBorder) # overlays the rectangle on top of the graph we drew earlier
+    regionHeight = maxRow - minRow
+    regionWidth = maxCol - minCol
+    # does the filtering
+    if regionHeight >= min_height and regionHeight <= max_height and regionWidth >= min_width and regionWidth <= max_width and regionHeight < regionWidth:
+        plate_like_objects.append(grayscale.binary_car_image[minRow:maxRow, minCol:maxCol])
+        plate_objects_cordinates.append((minRow, minCol, maxRow, maxCol))
+        rectBorder = patches.Rectangle((minCol, minRow), maxCol - minCol, maxRow - minRow, edgecolor = "red", linewidth = 2, fill = False)
+        # draws rectangle
+        ax1.add_patch(rectBorder) # overlays the rectangle on top of the graph we drew earlier
 
 plt.show()
